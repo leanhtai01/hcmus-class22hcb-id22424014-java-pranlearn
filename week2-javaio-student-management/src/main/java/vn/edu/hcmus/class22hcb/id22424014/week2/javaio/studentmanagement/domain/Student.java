1b72c5983df85c5c8b47f1024c3d291c5c945ea5
leanhtai01 <leanhtai01@gmail.com>
2023-05-28 16:11:37 +0700

    avoid boilerplate code by using lombok

--- week2-javaio-student-management/src/main/java/vn/edu/hcmus/class22hcb/id22424014/week2/javaio/studentmanagement/domain/Student.java
+++ week2-javaio-student-management/src/main/java/vn/edu/hcmus/class22hcb/id22424014/week2/javaio/studentmanagement/domain/Student.java
@@ -4,6 +4,15 @@ import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Student implements Serializable {
     @Serial
     private static final long serialVersionUID = 9082532603621712690L;
@@ -11,52 +20,11 @@ public class Student implements Serializable {
     private String id;
     private String name;
     private double grade;
+    private String address;
     private String note;
-
-    public Student() {
-    }
 
     public Student(String id) {
         this.id = id;
-    }
-
-    public Student(String id, String name, double grade, String note) {
-        this.id = id;
-        this.name = name;
-        this.grade = grade;
-        this.note = note;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getGrade() {
-        return grade;
-    }
-
-    public void setGrade(double grade) {
-        this.grade = grade;
-    }
-
-    public String getNote() {
-        return note;
-    }
-
-    public void setNote(String note) {
-        this.note = note;
     }
 
     @Override
