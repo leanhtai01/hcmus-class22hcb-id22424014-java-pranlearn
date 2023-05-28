3678e569ddac6a4405cfa6bc9e61ef5a01b4ce86
leanhtai01 <leanhtai01@gmail.com>
2023-05-28 19:04:08 +0700

    add method display menu

--- week2-javaio-student-management/src/main/java/vn/edu/hcmus/class22hcb/id22424014/week2/javaio/studentmanagement/ui/ConsoleUI.java
+++ week2-javaio-student-management/src/main/java/vn/edu/hcmus/class22hcb/id22424014/week2/javaio/studentmanagement/ui/ConsoleUI.java
@@ -1,14 +1,22 @@
 package vn.edu.hcmus.class22hcb.id22424014.week2.javaio.studentmanagement.ui;
 
-import static java.lang.invoke.MethodHandles.lookup;
-import static org.slf4j.LoggerFactory.getLogger;
-
-import org.slf4j.Logger;
-
 public class ConsoleUI {
-    private static final Logger LOGGER = getLogger(lookup().lookupClass());
-
     public static void main(String[] args) {
-        LOGGER.info("Console UI is running");
+        displayMenu();
+    }
+    
+    private static void displayMenu() {
+        System.console().writer().println("Menu option:");
+        System.console().writer().println("1. Add new student.");
+        System.console().writer().println("2. Update student.");
+        System.console().writer().println("3. Delete student.");
+        System.console().writer().println("4. Display students by ID ascending.");
+        System.console().writer().println("5. Display students by ID descending.");
+        System.console().writer().println("6. Display students by Grade ascending.");
+        System.console().writer().println("7. Display students by Grade descending.");
+        System.console().writer().println("8. Import data from CSV.");
+        System.console().writer().println("9. Export data to CSV.");
+        System.console().writer().println("0. Exit.");
+        System.console().writer().print("Please enter your choice: ");
     }
 }
